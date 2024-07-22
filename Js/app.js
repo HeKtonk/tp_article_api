d97999700d6ed9da08bfd283f6796567aee5adba
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-07-22 16:00:32 +0200

    Update app.js
    add a path to display

--- Js/app.js
+++ Js/app.js
@@ -5,6 +5,10 @@ app.get('/articles', (request, response) => {
     response.json( "Retournera la liste des articles");
 })
 
+app.get('/article/1', (request, response) => {
+    response.json("Retournera l'article ayant l'id 1");
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
