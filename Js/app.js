1d64ad2460c0dd2e0dc98c7b5613c868876427d2
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-07-22 16:02:13 +0200

    Update app.js
    Add path to create/update an article

--- Js/app.js
+++ Js/app.js
@@ -9,6 +9,10 @@ app.get('/article/1', (request, response) => {
     response.json("Retournera l'article ayant l'id 1");
 })
 
+app.get('/article/save-article', (request, response) => {
+    response.json("Va créer/mettre à jour un article envoyé");
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
