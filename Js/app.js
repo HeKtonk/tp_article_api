2980dff0298f7c1f0366accb85754ee3ddec8844
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-07-23 09:43:41 +0200

    Update app.js
    TP part 2
    Update app.js : Assign each route an action on the item object

--- Js/app.js
+++ Js/app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 
+let articles = [
+    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
+    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
+    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
+];
+
 app.get('/articles', (request, response) => {
-    response.json( "Retournera la liste des articles");
+    response.json(articles);
 })
 
 app.get('/article/:id', (request, response) => {
-    response.json(`Retournera l\'article ayant l\'id ${request.params.id}`);
+    response.json(JSON.stringify(articles[request.params.id]));
 })
 
 app.post('/article/save-article', (request, response) => {
     response.json("Va créer/mettre à jour un article envoyé");
+    articles.push(request.body);
 })
 
 app.delete('/article/:id', (request, response) => {
-    response.json(`Supprimera l\'article ayant l'id ${request.params.id}`);
+    response.json(`Supprimera l\'article ${JSON.stringify(articles[request.params.id])}`);
 })
 
 app.listen(3000, () => {
