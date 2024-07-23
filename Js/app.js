a28576eaa4263afe2cac5ac0c271d6ceb77cbc65
Corey Hz <153377078+HeKtonk@users.noreply.github.com>
2024-07-23 12:21:46 +0200

    Update app.js
    correction de la partie 2 du tp

--- Js/app.js
+++ Js/app.js
@@ -15,21 +15,59 @@ app.get('/article/:id', (request, response) => {
     response.json(JSON.stringify(articles[request.params.id]));
 })
 
-// parses incoming requests with JSON payloads
+// parses incoming requests with JSON payloads(body)
 app.use(express.json());
 app.post('/article/save-article', (request, response) => {
+    /*
     response.json("Va créer/mettre à jour un article envoyé");
     articles.push(request.body);
+    */
+
+    // On récupère le JSON
+    const articleJSON = request.body;
+
+    let foundArticle = null;
+
+    // Est-ce qu'on a un id envoyé dans le JSON
+    if(articleJSON.id != undefined || articleJSON.id ){
+        // Essayer de trouver un article existant
+        foundArticle = articles.find(article => article.id === articleJSON.id);
+    }
+
+    // Si je trouve je modifie
+    if(foundArticle){
+        foundArticle.title = articleJSON.title;
+        foundArticle.content = articleJSON.content;
+        foundArticle.author = articleJSON.author;
+
+        return response.json(`L'article à été modifié avec succès`);
+    }
+
+    // Sinon par défaut je crée
+    articles.push(articleJSON);
+    return response.json(`Article créé avec succès`)
 })
 
 app.delete('/article/:id', (request, response) => {
 
+    /*
     if(request.params.id > articles.length){
         response.json("Article inexistant");
     } else {
         response.json(`Supprime l\'article ${request.params.id}`);
         delete articles[request.params.id];
     }
+    */
+
+    // Il faut l'id en entier
+    const id = parseInt(request.params.id);
+
+    // Trouver l'index
+    const foundArticleIndex = articles.findIndex(article => article.id === id);
+
+    // Supprimer grace à  l'index
+    articles.slice(foundArticleIndex, 1);
+
 })
 
 app.listen(3000, () => {
